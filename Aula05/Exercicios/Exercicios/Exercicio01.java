12aad5d640b74f9fbe31ae8d63f2c7502214f628
Moises Briceño Medina <moisesbricenomedina29@gmail.com>
2023-09-26 00:16:44 -0300

    refatorando o nome do pacote Exercicicos

--- Aula05/Exercicios/Exercicios/Exercicio01.java
+++ Aula05/Exercicios/Exercicios/Exercicio01.java
@@ -1,4 +1,4 @@
-package Aula05.Exercicios.Exercicio01;
+package Aula05.Exercicios.Exercicios;
 import Aula05.Exercicios.utils.ScanUtils;
 public class Exercicio01 {
     public static void main(String[] args) {
--- Aula05/Exercicios/Exercicios/Exercicio02.java
+++ Aula05/Exercicios/Exercicios/Exercicio02.java
@@ -1,4 +1,4 @@
-package Aula05.Exercicios.Exercicio01;
+package Aula05.Exercicios.Exercicios;
 
 import Aula05.Exercicios.utils.ScanUtils;
 
