9c7184773db321a475378740517063b276a96514
moises briceno <moises.briceno@epicgames.com>
2023-09-27 15:39:48 -0300

    concluindo o exercicio03

--- Aula05/Exercicios/Exercicio03/TarefaTest.java
+++ Aula05/Exercicios/Exercicio03/TarefaTest.java
@@ -5,13 +5,45 @@ import Aula05.Exercicios.utils.ScanUtils;
 import java.util.ArrayList;
 import java.util.List;
 
+import static Aula05.Exercicios.utils.inputIndiceUtils.inputIndice;
+
 public class TarefaTest {
     public static void main(String[] args) {
         ScanUtils scan = new ScanUtils();
         List<Tarefa> armazenaTarefa = new ArrayList<>();
+        int opcao = 0;
+
+        while (opcao != 4) {
+            System.out.printf("""
+                    Escolha uma opcão:
+                    1 - Adicionar tarefa
+                    2 - Remover tarefa
+                    3 - Exibir tarefas
+                    4 - Fechar
+                    """);
+            opcao = scan.coletaInt("");
+
+            switch (opcao) {
+                case 1:
+                    adicionarTarefa(armazenaTarefa, scan);
+                    break;
+                case 2:
+                    if (verificaTarefas(armazenaTarefa)) {
+                        removerTarefas(armazenaTarefa, scan);
+                    }
+                    break;
+                case 3:
+                    if (verificaTarefas(armazenaTarefa)) {
+                        exibirTarefas(armazenaTarefa);
+                    }
+                    break;
+                case 4:
+                    System.out.println("Fechadno o Programa");
+            }
+        }
     }
 
-    public void adicionartarefa(List<Tarefa> listaTarefas, ScanUtils scan) {
+    public static void adicionarTarefa(List<Tarefa> listaTarefas, ScanUtils scan) {
         String nome = scan.coletaTexto("Informe o nome da tarefa");
         String descricao = scan.coletaTexto("Informe a descricao da tarefa");
         String data = scan.coletaTexto("Informe a data da tarefa use dia/mes/ano");
@@ -22,19 +54,20 @@ public class TarefaTest {
         System.out.println("Tarefa criada!!");
     }
 
-    public void removertarefas(List<Tarefa> listaTarefas){
-
+    public static void removerTarefas(List<Tarefa> listaTarefas, ScanUtils scan) {
+        listaTarefas.remove(inputIndice(listaTarefas, scan));
+        System.out.printf("Tarefa criada!");
     }
 
-    public void exibirTarefas(List<Tarefa> listaTarefas) {
+    public static void exibirTarefas(List<Tarefa> listaTarefas) {
         System.out.println("Tarefas adicionadas");
         for (Tarefa tarefa : listaTarefas) {
             System.out.println(tarefa);
         }
     }
-    public boolean verificaTarefas(List<Tarefa> listaTarefas) {
+
+    public static boolean verificaTarefas(List<Tarefa> listaTarefas) {
         if (!listaTarefas.isEmpty()) {
-            System.out.println("Nenhuma tarefa encontrada.");
             return true;
         } else
             System.out.println("Lista de tarefas vazia");
