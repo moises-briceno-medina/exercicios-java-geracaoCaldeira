53700311fa87097e99995d69db77b73fb972c64b
Moises Briceño Medina <moisesbricenomedina29@gmail.com>
2023-09-28 15:50:59 -0300

    cirando a classe "impressao"

---
+++ Aula05/Exercicios/exercicio05/Impressao.java
@@ -0,0 +1,11 @@
+package Aula05.Exercicios.exercicio05;
+
+public class Impressao {
+    private String nome;
+    private int numeroDePagina;
+
+    public Impressao(String nome , int numeroDePagina){
+        this.nome = nome;
+        this.numeroDePagina = numeroDePagina;
+    }
+}
