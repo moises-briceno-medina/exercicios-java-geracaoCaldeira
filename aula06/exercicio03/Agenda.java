8ad7b719cb2cb20763f7c8802b8704c4afafb235
Moises Briceño Medina <moisesbricenomedina29@gmail.com>
2023-10-01 18:18:20 -0300

    criando o construtor da classe agenda

--- aula06/exercicio03/Agenda.java
+++ aula06/exercicio03/Agenda.java
@@ -1,3 +1,9 @@
+//Exercício 3: TreeMap com agenda
+//Crie um programa Java que utilize uma estrutura de dados do tipo TreeMap
+// para criar uma agenda telefônica. Agenda deve conter: número de telefone,
+// nome, endereço e local da cidade (norte, sul, centro, leste e oeste).
+// Associe nomes a números de telefone e, em seguida, imprima a lista de
+// contatos em ordem alfabética e imprima os contatos com base na localidade.
 package aula06.exercicio03;
 
 public class Agenda {
@@ -6,5 +12,10 @@ public class Agenda {
     private String endereco;
     private String localDaCidade;
 
-    
+    public Agenda(int numeroDeTelefono, String nome, String endereco, String localDaCidade) {
+        this.numeroDeTelefono = numeroDeTelefono;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.localDaCidade = localDaCidade;
+    }
 }
