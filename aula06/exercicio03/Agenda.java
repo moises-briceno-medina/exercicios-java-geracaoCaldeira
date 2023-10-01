888628382c055b311754b49746fd88f883720ab1
Moises Briceño Medina <moisesbricenomedina29@gmail.com>
2023-10-01 18:33:16 -0300

    criando a classe Exercicio03 e adicionando um TreepMap

--- aula06/exercicio03/Agenda.java
+++ aula06/exercicio03/Agenda.java
@@ -1,21 +1,17 @@
-//Exercício 3: TreeMap com agenda
-//Crie um programa Java que utilize uma estrutura de dados do tipo TreeMap
-// para criar uma agenda telefônica. Agenda deve conter: número de telefone,
-// nome, endereço e local da cidade (norte, sul, centro, leste e oeste).
-// Associe nomes a números de telefone e, em seguida, imprima a lista de
-// contatos em ordem alfabética e imprima os contatos com base na localidade.
+
 package aula06.exercicio03;
 
 public class Agenda {
-    private int numeroDeTelefono;
+    private String numeroDeTelefono;
     private String nome;
     private String endereco;
     private String localDaCidade;
 
-    public Agenda(int numeroDeTelefono, String nome, String endereco, String localDaCidade) {
+    public Agenda(String numeroDeTelefono, String nome, String endereco, String localDaCidade) {
         this.numeroDeTelefono = numeroDeTelefono;
         this.nome = nome;
         this.endereco = endereco;
         this.localDaCidade = localDaCidade;
     }
+
 }
--- aula06/exercicio03/Exercicio03.java
+++ aula06/exercicio03/Exercicio03.java
@@ -1,2 +1,21 @@
-package aula06.exercicio03;public class Exercicio03 {
+//Exercício 3: TreeMap com agenda
+//Crie um programa Java que utilize uma estrutura de dados do tipo TreeMap
+// para criar uma agenda telefônica. Agenda deve conter: número de telefone,
+// nome, endereço e local da cidade (norte, sul, centro, leste e oeste).
+// Associe nomes a números de telefone e, em seguida, imprima a lista de
+// contatos em ordem alfabética e imprima os contatos com base na localidade.
+package aula06.exercicio03;
+
+import aula01.utils.ScanUtils;
+
+import java.util.TreeMap;
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+        ScanUtils scan = new ScanUtils();
+        TreeMap<String, String> AgendaTelefonica = new TreeMap<>();
+
+    }
+
+    
 }
