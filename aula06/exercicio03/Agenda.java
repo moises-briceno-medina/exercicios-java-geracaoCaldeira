1a03ecd40e1caaa736bafffa96bffab24ad19d90
Moises Briceño Medina <moisesbricenomedina29@gmail.com>
2023-10-01 18:14:00 -0300

    criando os atributos da classe

--- aula06/exercicio03/Agenda.java
+++ aula06/exercicio03/Agenda.java
@@ -1,5 +1,10 @@
 package aula06.exercicio03;
 
 public class Agenda {
+    private int numeroDeTelefono;
+    private String nome;
+    private String endereco;
+    private String localDaCidade;
+
     
 }
