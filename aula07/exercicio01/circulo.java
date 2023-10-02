a1788a9a98e3b3d45efb9d01f8c6119badf11a8d
Moises Briceño Medina <moisesbricenomedina29@gmail.com>
2023-10-02 00:25:27 -0300

    implementando a interface Desenhar e o seu metodo na classe Circulo

--- aula07/exercicio01/circulo.java
+++ aula07/exercicio01/circulo.java
@@ -1,4 +1,8 @@
 package aula07.exercicio01;
 
-public class circulo {
+public class circulo implements Desenhavel{
+    @Override
+    public void desenhar() {
+        System.out.println("Vou desenhar um circulo bonito");
+    }
 }
