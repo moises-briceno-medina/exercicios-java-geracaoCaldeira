29f92c48e7ba618d26789d0e1fa586ebe6d3b4f8
Moises Briceño Medina <moisesbricenomedina29@gmail.com>
2023-10-02 00:27:33 -0300

    criando a classe Quadrado e implementando a interface Desenhar e o seu metodo na classe Quadrado

---
+++ aula07/exercicio01/Quadrado.java
@@ -0,0 +1,9 @@
+package aula07.exercicio01;
+
+public class Quadrado implements Desenhavel {
+
+    @Override
+    public void desenhar() {
+        System.out.println("Vou desenhar um Quadrado lindo");
+    }
+}
