3859d897b0c7422db7a36398c9bd2657e5f22b4f
Moises Briceño Medina <moisesbricenomedina29@gmail.com>
2023-10-01 23:29:57 -0300

    Criando o pacote exercicio04 e a classe Produto com os atributos nome e valor e seu construtor

---
+++ aula06/exercicio04/Produto.java
@@ -0,0 +1,12 @@
+package aula06.exercicio04;
+
+public class Produto {
+    private String nome;
+    private int valor;
+
+    public Produto(String nome, int valor) {
+        this.nome = nome;
+        this.valor = valor;
+    }
+    
+}
