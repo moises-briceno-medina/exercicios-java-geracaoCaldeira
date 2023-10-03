feb7e2440a9c729f5124367fd4427c6a9f4f1620
Moises Briceño Medina <moisesbricenomedina29@gmail.com>
2023-10-03 11:30:38 -0300

    adcionando o atributo saldo e o construtor e metodoGett

--- aula07/Exercicio06/ContaBancaria.java
+++ aula07/Exercicio06/ContaBancaria.java
@@ -1,4 +1,13 @@
 package aula07.Exercicio06;
 
 public class ContaBancaria {
+    private double saldo;
+
+    public ContaBancaria() {
+        this.saldo = 100;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 }
