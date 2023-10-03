eb4b95a7ef3f88845d53ec114804d6cf387da1e2
Moises Briceño Medina <moisesbricenomedina29@gmail.com>
2023-10-03 11:38:31 -0300

    adicionando o metodo saque

--- aula07/Exercicio06/ContaBancaria.java
+++ aula07/Exercicio06/ContaBancaria.java
@@ -10,4 +10,12 @@ public class ContaBancaria {
     public double getSaldo() {
         return saldo;
     }
+    public void sacar (double valor) throws SaldoInsuficienteException {
+        if (this.saldo >= valor){
+            this.saldo -= valor;
+            System.out.println("Saque feito com sucesso");
+        }else {
+            throw new SaldoInsuficienteException("Saldo insuficiente");
+        }
+    }
 }
