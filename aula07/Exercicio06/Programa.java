8eca543f7d6e94d6ff97608d48d0be6526b11f7f
Moises Briceño Medina <moisesbricenomedina29@gmail.com>
2023-10-03 11:39:08 -0300

    adicionando a classe Programa

---
+++ aula07/Exercicio06/Programa.java
@@ -0,0 +1,10 @@
+package aula07.Exercicio06;
+
+public class Programa {
+    public static void main(String[] args) throws SaldoInsuficienteException {
+        ContaBancaria conta1 = new ContaBancaria();
+
+        conta1.sacar(50);
+    }
+
+}
