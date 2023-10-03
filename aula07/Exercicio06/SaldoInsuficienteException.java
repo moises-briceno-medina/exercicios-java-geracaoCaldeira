8ff4dc5583806fe2c3aada16f82ee18f01a5da42
Moises Briceño Medina <moisesbricenomedina29@gmail.com>
2023-10-03 11:34:41 -0300

    criando a excepcao personalizada

--- aula07/Exercicio06/SaldoInsuficienteException.java
+++ aula07/Exercicio06/SaldoInsuficienteException.java
@@ -2,4 +2,7 @@ package aula07.Exercicio06;
 
 public class SaldoInsuficienteException extends Exception{
 
+    public SaldoInsuficienteException(String mensagem){
+        super(mensagem);
+    }
 }
