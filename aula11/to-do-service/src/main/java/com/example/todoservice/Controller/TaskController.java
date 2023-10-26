cc6d979e6d7400b6153694bcc9add2689f235481
Moises Briceño Medina <moisesbricenomedina29@gmail.com>
2023-10-26 16:48:55 -0300

    aula 11 to-do-server

---
+++ aula11/to-do-service/src/main/java/com/example/todoservice/Controller/TaskController.java
@@ -0,0 +1,9 @@
+package com.example.todoservice.Controller;
+
+import com.example.todoservice.model.Task;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskController {
+    List<Task> listTask = new ArrayList<>();
+}
---
+++ aula11/to-do-service/src/main/java/com/example/todoservice/model/Task.java
@@ -0,0 +1,10 @@
+package com.example.todoservice.model;
+
+import java.time.LocalDate;
+
+public class Task {
+    private int id;
+    private String descricao;
+    private LocalDate dataDeVencimento;
+    private boolean estado;
+}
